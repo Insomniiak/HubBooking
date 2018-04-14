b586f1bad3aa19d2f01a48d3240e35fa82f812cc
Insomniak <aymeric.beudaert@gmail.com>
2018-04-14 14:32:37 +0200

    linterrr

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,17 @@
 module.exports = {
-    "extends": "airbnb"
+	"env": {
+		"browser": true,
+		"es6": true,
+		"node": true
+	},
+	"extends": "airbnb",
+	"parser": "babel-eslint",
+	"rules": {
+		"indent": [2, "tab", { "SwitchCase": 1, "VariableDeclarator": 1 }],
+		"no-tabs": 0,
+		"react/prop-types": 0,
+		"react/jsx-indent": [2, "tab"],
+		"react/jsx-indent-props": [2, "tab"],
+		"react/jsx-filename-extension": ["error", { "extensions": [".js", ".jsx"] }]
+	}
 };
