fa5e77b85cc4605fec6c9693c6c19f224ec2845f
aymeric <aymeric.beudaert@epitech.eu>
2018-04-16 17:56:44 +0200

    CHANGE TITLE NAV
    Adding HubBooking by default for all navigations options
    
    Signed-off-by: aymeric <aymeric.beudaert@epitech.eu>

--- src/components/MainNavigation.tsx
+++ src/components/MainNavigation.tsx
@@ -1,10 +1,18 @@
 import { StackNavigator } from 'react-navigation';
 import HomeScreen from '../views/HomeScreen';
 
-const RootStack = StackNavigator({
-	Home: {
-		screen: HomeScreen,
-	},
-});
+const RootStack = StackNavigator(
+    {
+        Home: {
+            screen: HomeScreen,
+        },
+    },
+    {
+        navigationOptions: (): object => ({
+                title: "HubBooking",
+            }
+        )
+    }
+);
 
 export default RootStack;
